b03331d038b1134ce2e3bd449268a88718f35a1b
csixty4 <csixty4@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-03-20 01:53:04 +0000

    Added json_encode implementation for PHP < 5.2.0
    git-svn-id: https://plugins.svn.wordpress.org/daves-wordpress-live-search/trunk@103619 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- daves-wordpress-live-search-ajax.php
+++ daves-wordpress-live-search-ajax.php
@@ -17,6 +17,53 @@
  * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  **/
 
+if (!function_exists('json_encode'))
+{
+	function json_encode($a=false)
+	{
+		if (is_null($a)) return 'null';
+		if ($a === false) return 'false';
+		if ($a === true) return 'true';
+		if (is_scalar($a))
+		{
+			if (is_float($a))
+			{
+			  // Always use "." for floats.
+			  return floatval(str_replace(",", ".", strval($a)));
+			}
+		
+			if (is_string($a))
+			{
+				static $jsonReplaces = array(array("\\", "/", "\n", "\t", "\r", "\b", "\f", '"'), array('\\\\', '\\/', '\\n', '\\t', '\\r', '\\b', '\\f', '\"'));
+				return '"' . str_replace($jsonReplaces[0], $jsonReplaces[1], $a) . '"';
+			}
+			else
+				return $a;
+		}
+		
+		$isList = true;
+		for ($i = 0, reset($a); $i < count($a); $i++, next($a))
+		{
+			if (key($a) !== $i)
+			{
+				$isList = false;
+				break;
+			}
+		}
+		$result = array();
+		if ($isList)
+		{
+			foreach ($a as $v) $result[] = json_encode($v);
+			return '[' . join(',', $result) . ']';
+		}
+		else
+		{
+			foreach ($a as $k => $v) $result[] = json_encode($k).':'.json_encode($v);
+			return '{' . join(',', $result) . '}';
+		}
+	}
+}
+
 include_once("../../../wp-config.php");
 
 $wp_query = new WP_Query(array('s' => $_GET['s'], 'showposts' => 100));
