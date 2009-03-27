65e111d8b43f5f2674aef8701254a7665cc1985f
csixty4 <csixty4@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-03-27 05:28:59 +0000

    Admin screen w/Max Results
    git-svn-id: https://plugins.svn.wordpress.org/daves-wordpress-live-search/trunk@105628 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- daves-wordpress-live-search.php
+++ daves-wordpress-live-search.php
@@ -26,10 +26,16 @@ Plugin URI: http://wordpress.org/extend/plugins/daves-wordpress-live-search/
  * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  **/
  
+// Register hooks
 add_action('init', array('DavesWordPressLiveSearch', 'advanced_search_init'));
+add_action('admin_menu', array('DavesWordPressLiveSearch', 'admin_menu'));
 
 class DavesWordPressLiveSearch
 {
+	///////////////////
+	// Initialization
+	///////////////////
+	
 	public static function advanced_search_init()
 	{
 		$pluginPath = WP_PLUGIN_URL.'/'.str_replace(basename(__FILE__),"",plugin_basename(__FILE__));
@@ -39,6 +45,44 @@ class DavesWordPressLiveSearch
 		wp_enqueue_script('daves-wordpress-live-search', $pluginPath.'daves-wordpress-live-search.js.php', 'jquery_dimensions');
 		
 		wp_enqueue_style( 'daves-wordpress-live-search', $pluginPath.'daves-wordpress-live-search.css');
+	}
+	
+	///////////////
+	// Admin Pages
+	///////////////
+	
+	public function admin_menu()
+	{
+		add_options_page("Dave's WordPress Live Search Options", "Live Search", 8, __FILE__, array('DavesWordPressLiveSearch', 'plugin_options'));	
+	}
+	
+	public function plugin_options()
+	{
+		$thisPluginsDirectory = dirname(__FILE__);
+		
+		if($_POST['daves-wordpress-live-search_submit'] == "Save Changes")
+		{
+			// Read their posted value
+	        $maxResults = max(intval($_POST['daves-wordpress-live-search_max_results']), 0);
+
+	        // Save the posted value in the database
+	        update_option('daves-wordpress-live-search_max_results', $maxResults );	
+	        
+	        // Translate the "Options saved" message...just in case.
+	        // You know...the code I was copying for this does it, thought it might be a good idea to leave it
+	        $updateMessage = __('Options saved.', 'mt_trans_domain' );
+	        
+	        echo "<div class=\"updated\"><p><strong>$updateMessage</strong></p></div>";
+		}
+		else
+		{
+			$maxResults = intval(get_option('daves-wordpress-live-search_max_results'));
+		}
+		
+		$template = file_get_contents("$thisPluginsDirectory/daves-wordpress-live-search-admin.tpl");
+		$template = str_replace('{MAX_RESULTS}', stripslashes($maxResults), $template);
+		
+		echo $template;
 	}
 }
 ?>
