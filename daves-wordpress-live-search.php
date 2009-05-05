63ffdc523bd097e2891967f7fbe40d7cd0c32beb
csixty4 <csixty4@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-05-05 01:58:31 +0000

    PHP version check
    git-svn-id: https://plugins.svn.wordpress.org/daves-wordpress-live-search/trunk@115596 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- daves-wordpress-live-search.php
+++ daves-wordpress-live-search.php
@@ -31,6 +31,9 @@ add_action('init', array('DavesWordPressLiveSearch', 'advanced_search_init'));
 add_action('admin_menu', array('DavesWordPressLiveSearch', 'admin_menu'));
 add_action('wp_head', array('DavesWordPressLiveSearch', 'head'));
 
+if(5.0 > floatval(phpversion()))
+	die("Dave's WordPress Live Search requires PHP 5.0 or higher");
+
 // Pre-2.6 compatibility
 // See http://codex.wordpress.org/Determining_Plugin_and_Content_Directories
 if ( ! defined( 'WP_CONTENT_URL' ) )
@@ -42,73 +45,5 @@ if ( ! defined( 'WP_PLUGIN_URL' ) )
 if ( ! defined( 'WP_PLUGIN_DIR' ) )
       define( 'WP_PLUGIN_DIR', WP_CONTENT_DIR . '/plugins' );
 
-class DavesWordPressLiveSearch
-{
-	///////////////////
-	// Initialization
-	///////////////////
-	
-	public static function advanced_search_init()
-	{
-		$pluginPath = WP_PLUGIN_URL.'/'.str_replace(basename(__FILE__),"",plugin_basename(__FILE__));
-		
-		wp_enqueue_script('jquery');
-		wp_enqueue_script('jquery_dimensions', $pluginPath.'jquery.dimensions.pack.js', 'jquery');
-	}
-	
-	public static function head()
-	{
-		$pluginPath = WP_PLUGIN_URL.'/'.str_replace(basename(__FILE__),"",plugin_basename(__FILE__));		
-		$thisPluginsDirectory = dirname(__FILE__);
-		
-		//$css = file_get_contents($thisPluginsDirectory.'/daves-wordpress-live-search.css.tpl');
-		//$css = str_replace("\n", "", $css);
-		//echo $css;
-		include($thisPluginsDirectory.'/daves-wordpress-live-search.css.tpl');
-
-		include($thisPluginsDirectory.'/daves-wordpress-live-search.js.php');
-		
-	}
-	
-	///////////////
-	// Admin Pages
-	///////////////
-	
-	public function admin_menu()
-	{
-		add_options_page("Dave's WordPress Live Search Options", "Live Search", 8, __FILE__, array('DavesWordPressLiveSearch', 'plugin_options'));	
-	}
-	
-	public function plugin_options()
-	{
-		$thisPluginsDirectory = dirname(__FILE__);
-		
-		if($_POST['daves-wordpress-live-search_submit'] == "Save Changes")
-		{
-			// Read their posted value
-	        $maxResults = max(intval($_POST['daves-wordpress-live-search_max_results']), 0);
-	        $resultsDirection = $_POST['daves-wordpress-live-search_results_direction'];
-
-	        // Save the posted value in the database
-	        update_option('daves-wordpress-live-search_max_results', $maxResults );	
-	        update_option('daves-wordpress-live-search_results_direction', $resultsDirection);
-	        
-	        // Translate the "Options saved" message...just in case.
-	        // You know...the code I was copying for this does it, thought it might be a good idea to leave it
-	        $updateMessage = __('Options saved.', 'mt_trans_domain' );
-	        
-	        echo "<div class=\"updated\"><p><strong>$updateMessage</strong></p></div>";
-		}
-		else
-		{
-			$maxResults = intval(get_option('daves-wordpress-live-search_max_results'));
-			$resultsDirection = stripslashes(get_option('daves-wordpress-live-search_results_direction'));
-		}
-	        
-	    if(!in_array($resultsDirection, array('up', 'down')))
-	        	$resultsDirection = 'down';
-	        		
-		include("$thisPluginsDirectory/daves-wordpress-live-search-admin.tpl");
-	}
-}
+include_once("DavesWordPressLiveSearch.php");
 ?>
