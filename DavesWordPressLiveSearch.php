d1113370c8f5011eb20d5a9109fef6be0d176ca5
csixty4 <csixty4@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-05-22 06:18:07 +0000

    optimization & documentation
    git-svn-id: https://plugins.svn.wordpress.org/daves-wordpress-live-search/trunk@119597 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- DavesWordPressLiveSearch.php
+++ DavesWordPressLiveSearch.php
@@ -5,6 +5,10 @@ class DavesWordPressLiveSearch
 	// Initialization
 	///////////////////
 	
+	/**
+	 * Initialize the live search object & enqueuing scripts
+	 * @return void
+	 */
 	public static function advanced_search_init()
 	{
 		$pluginPath = WP_PLUGIN_URL.'/'.str_replace(basename(__FILE__),"",plugin_basename(__FILE__));
@@ -13,39 +17,48 @@ class DavesWordPressLiveSearch
 		wp_enqueue_script('jquery_dimensions', $pluginPath.'jquery.dimensions.pack.js', 'jquery');
 	}
 	
+	/**
+	 * Output the plugin's CSS & Javascript into the page's <head>
+	 * @return void 
+	 */
 	public static function head()
-	{
-		$pluginPath = WP_PLUGIN_URL.'/'.str_replace(basename(__FILE__),"",plugin_basename(__FILE__));		
+	{		
 		$thisPluginsDirectory = dirname(__FILE__);
 		
-		//$css = file_get_contents($thisPluginsDirectory.'/daves-wordpress-live-search.css.tpl');
-		//$css = str_replace("\n", "", $css);
-		//echo $css;
-		include($thisPluginsDirectory.'/daves-wordpress-live-search.css.tpl');
-
-		include($thisPluginsDirectory.'/daves-wordpress-live-search.js.php');
+		// $pluginPath is used in the Javascript
+		$pluginPath = WP_PLUGIN_URL.'/'.str_replace(basename(__FILE__),"",plugin_basename(__FILE__));
 		
+		include($thisPluginsDirectory.'/daves-wordpress-live-search.css.tpl');
+		include($thisPluginsDirectory.'/daves-wordpress-live-search.js.php');
 	}
 	
 	///////////////
 	// Admin Pages
 	///////////////
 	
+	/**
+	 * Include the Live Search options page in the admin menu
+	 * @return void
+	 */
 	public function admin_menu()
 	{
-		add_options_page("Dave's WordPress Live Search Options", "Live Search", 8, __FILE__, array('DavesWordPressLiveSearch', 'plugin_options'));	
+		add_options_page("Dave's WordPress Live Search Options", __('Live Search', 'mt_trans_domain'), 8, __FILE__, array('DavesWordPressLiveSearch', 'plugin_options'));	
 	}
 	
+	/**
+	 * Display & process the Live Search admin options
+	 * @return void
+	 */
 	public function plugin_options()
 	{
 		$thisPluginsDirectory = dirname(__FILE__);
 		
-		if($_POST['daves-wordpress-live-search_submit'] == "Save Changes")
+		if("Save Changes" == $_POST['daves-wordpress-live-search_submit'])
 		{
 			// Read their posted value
 	        $maxResults = max(intval($_POST['daves-wordpress-live-search_max_results']), 0);
 	        $resultsDirection = $_POST['daves-wordpress-live-search_results_direction'];
-	        $displayPostMeta = ($_POST['daves-wordpress-live-search_display_post_meta'] == "true");
+	        $displayPostMeta = ("true" == $_POST['daves-wordpress-live-search_display_post_meta']);
 
 	        // Save the posted value in the database
 	        update_option('daves-wordpress-live-search_max_results', $maxResults );	
