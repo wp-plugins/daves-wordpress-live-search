873aa3b1cefbbc9f0e58c7f6382957c9fb6ab322
csixty4 <csixty4@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-08-02 04:59:20 +0000

    Moved i18n stuff to a "languages" directory
    git-svn-id: https://plugins.svn.wordpress.org/daves-wordpress-live-search/trunk@418172 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- DavesWordPressLiveSearch.php
+++ DavesWordPressLiveSearch.php
@@ -46,7 +46,7 @@ class DavesWordPressLiveSearch {
             }
         }
         
-        load_plugin_textdomain('dwls', false, dirname( plugin_basename( __FILE__ ) ) );
+        load_plugin_textdomain('dwls', false, dirname( plugin_basename( __FILE__ ) . '/languages/' ) );
     }
 
     public static function head() {
