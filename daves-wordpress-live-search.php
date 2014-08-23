127cc5cc37e213068fe20e098bfe1ca0c0138d5e
csixty4 <csixty4@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-23 14:41:41 +0000

    Updated copyright dates
    git-svn-id: https://plugins.svn.wordpress.org/daves-wordpress-live-search/trunk@971387 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- daves-wordpress-live-search.php
+++ daves-wordpress-live-search.php
@@ -10,7 +10,7 @@ Plugin URI: http://wordpress.org/extend/plugins/daves-wordpress-live-search/
 */
 
 /**
- * Copyright (c) 2009 Dave Ross <dave@csixty4.com>
+ * Copyright (c) 2014 Dave Ross <dave@csixty4.com>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
  * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
